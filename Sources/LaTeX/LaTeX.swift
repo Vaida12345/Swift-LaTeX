a2578f996e3d7effcd0e33bc4104d9750cef9b5f
Vaida <91354917+Vaida12345@users.noreply.github.com>
2022-10-18 17:46:49 +1100

    Initial Commit

---
+++ Sources/LaTeX/LaTeX.swift
@@ -0,0 +1,6 @@
+public struct LaTeX {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/LaTeXTests/LaTeXTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LaTeX
+
+final class LaTeXTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LaTeX().text, "Hello, World!")
+    }
+}
