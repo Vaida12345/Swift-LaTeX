82c1b1db1afe0478c577ac91ff664964173a14ba
Vaida <91354917+Vaida12345@users.noreply.github.com>
2022-10-20 19:12:32 +1100

    update

---
+++ Sources/LaTeX/LaTeXDSL/Components/Operators.swift
@@ -0,0 +1,82 @@
+//
+//  Operators.swift
+//  
+//
+//  Created by Vaida on 10/20/22.
+//
+
+
+public extension LaTeXComponent {
+    
+    @available(*, unavailable, message: "Use Addition instead.")
+    static func + (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: plus, rhs: rhs)
+    }
+    
+    @available(*, unavailable, message: "Use Subtraction instead.")
+    static func - (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: minus, rhs: rhs)
+    }
+    
+    @available(*, unavailable, message: "Use Multiplication instead.")
+    static func * (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: times, rhs: rhs)
+    }
+    
+    @available(*, unavailable, message: "Use Division instead.")
+    static func / (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: divide, rhs: rhs)
+    }
+    
+    @available(*, unavailable, message: "Use Equals instead.")
+    static func == (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: equal, rhs: rhs)
+    }
+    
+    @available(*, unavailable, message: "Use Equals and `not` instead.")
+    static func != (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: equal.not, rhs: rhs)
+    }
+    
+    static func > (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: greater, rhs: rhs)
+    }
+    
+    static func >= (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: greaterEqual, rhs: rhs)
+    }
+    
+    static func < (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: less, rhs: rhs)
+    }
+    
+    static func <= (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: lessEqual, rhs: rhs)
+    }
+    
+    static func ^ (lhs: Self, rhs: some LaTeXComponent) -> some LaTeXComponent {
+        BinaryComponent(lhs: lhs, operator: "^", rhs: rhs)
+    }
+    
+}
+
+
+public func Addition(@LaTeXBuilder lhs: () -> some LaTeXComponent, @LaTeXBuilder with rhs: () -> some LaTeXComponent) -> some LaTeXComponent {
+    BinaryComponent(lhs: lhs(), operator: plus, rhs: rhs())
+}
+
+public func Subtraction(@LaTeXBuilder lhs: () -> some LaTeXComponent, @LaTeXBuilder by rhs: () -> some LaTeXComponent) -> some LaTeXComponent {
+    BinaryComponent(lhs: lhs(), operator: minus, rhs: rhs())
+}
+
+public func Multiplication(@LaTeXBuilder lhs: () -> some LaTeXComponent, @LaTeXBuilder by rhs: () -> some LaTeXComponent) -> some LaTeXComponent {
+    BinaryComponent(lhs: lhs(), operator: times, rhs: rhs())
+}
+
+public func Division(@LaTeXBuilder lhs: () -> some LaTeXComponent, @LaTeXBuilder by rhs: () -> some LaTeXComponent) -> some LaTeXComponent {
+    BinaryComponent(lhs: lhs(), operator: divide, rhs: rhs())
+}
+
+public func Equals(@LaTeXBuilder lhs: () -> some LaTeXComponent, @LaTeXBuilder with rhs: () -> some LaTeXComponent) -> some LaTeXComponent {
+    BinaryComponent(lhs: lhs(), operator: equal, rhs: rhs())
+}
