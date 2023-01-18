f8c7e0a557909cea57b3ffe5d09a4553faa74234
Vaida <91354917+Vaida12345@users.noreply.github.com>
2023-01-19 03:03:11 +1100

    update

--- Tests/LaTeXTests/LaTeXTests.swift
+++ Tests/LaTeXTests/LaTeXTests.swift
@@ -5,6 +5,8 @@ import Foundation
 
 final class LaTesTs: XCTestCase {
     func testExample() async throws {
-        
+        print(Integral("t", from: "-\\infty", to: "x", body: { variable in
+            "f(t)"
+        }).latexExpression)
     }
 }
